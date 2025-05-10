7c73f93bd0dfd88662a0d18049009cd7fb3ae2f2
DeveloperXPK <andresgiraldo05a@gmail.com>
2025-05-09 23:04:48 -0500

    Eliminar archivos y configuraciones del proyecto, incluyendo modelos, vistas, formularios, plantillas y archivos de migración. Se eliminan también dependencias de requirements.txt y la base de datos SQLite. Se actualizan las configuraciones de Django para reflejar estos cambios.

--- milenio/urls.py
+++ milenio/urls.py
@@ -2,7 +2,7 @@
 URL configuration for milenio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('public_web.urls')),
-    path('dashboard/', include('admin_panel.urls')),
-    path('accounts/login/', LoginView.as_view(template_name='admin_panel/login.html'), name='login'),
-    path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
