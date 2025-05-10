72057226c406b035bded1761da7bdddc1016348c
PipeG222 <92407434+PipeG222@users.noreply.github.com>
2025-05-09 23:36:44 -0500

    Actualizar configuración de Django y mejorar la navegación. Se añaden ajustes de seguridad, se implementa Jazzmin para la interfaz administrativa, y se actualizan las URLs para la autenticación de usuarios.

--- milenio/urls.py
+++ milenio/urls.py
@@ -22,5 +22,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), 
+
     path('', include('public_web.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
